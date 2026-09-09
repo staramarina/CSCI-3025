9fb817f3de6d921b52cd733602a08d80b5b515e8
Tiffany Mui <tiffany_mui934@sunyempire.edu>
2026-09-09 04:16:14 -0600

    added boolean expressions

--- Module2/Mui_M2Assignment.py
+++ Module2/Mui_M2Assignment.py
@@ -21,10 +21,11 @@ quotient43 = num4 / num3        #division of decimal types
 #string manipulations
 stringjoin1 = ' '.join([string1, "It's fun!"])              #add another string to the end of string1 with a space between strings
 slicedstring1 = string1[12:17]                              #slice string1 to isolate "Python"
-slicedstring2 = stringjoin1[25:29]                          #slice stringjoin to isolate "fun!"
+slicedstring2 = stringjoin1[25:29]                          #slice stringjoin1 to isolate "fun!"
 stringjoin2 = slicedstring1 + " is " + slicedstring2        #join sliced strings with " is " in between
 
 #boolean expressions
-
+bool1 = num4 > num3             #check if num4 is greater than num3, should be true
+bool2 = len(stringjoin1) >= 28  #check if the length of stringjoin1 is greater than or equal to 28, should be true
 
 #output statements
