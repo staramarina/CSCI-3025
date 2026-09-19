5e28fd6967ecad277e9199dc85adc7abc71cee10
Tiffany Mui <tiffany_mui934@sunyempire.edu>
2026-09-19 06:26:54 -0600

    added part two which is a for loop to evaluate number of vowels in a user given string

--- Module3/Mui_M3Assignment.py
+++ Module3/Mui_M3Assignment.py
@@ -9,10 +9,10 @@ tryagain = True                                                                 
 while True == tryagain:                                                         # while loop to continue attempting to get correct user input
     try:                                                                        # try statement to check that user input is correct format
         score = float(score)                                                    # attempt to convert input value to a float type
-        tryagain = False                                                        # attempt was successful, so set the try again flag to false
+        tryagain = False                                                        # attempt successful; set try again flag to false, exit while loop
     except:                                                                     # exception handles when user input is not in correct format
         print("Error processing input, please try again.")                      # tells the operator that the input could not be processed
-        score = input("Enter a percentage score (do not include '%'): ")        # attempt to get operator input again
+        score = input("Enter a percentage score (do not include '%'): ")        # attempt to get operator input again, continue while loop
 if score >= 90.0:                                                               # if the score is greater than or equal to 90
     print(f"A score of {score}% results in grade A.")                           # print A grade and exit conditional statement
 elif score >= 80:                                                               # if the score is greater than or equal to 80
@@ -24,4 +24,15 @@ elif score >= 60:                                                               
 else:                                                                           # if the score is less than 60
     print(f"A score of {score}% results in grade F.")                           # print F grade and exit conditional statement
 
-# Part 2: 
+# Part 2: for loop to evaluate number of vowels in a string
+words = input("\nEnter a statement:\n")                                         # prompt user to enter a statement
+vowels = ["a", "e", "i", "o", "u"]                                              # list of vowels to check against
+vowelcount = 0                                                                  # initiate counter to hold the number of vowels detected
+if 0 == len(words):                                                             # check for blank input
+    print("There are no vowels in a blank statement.")                          # inform user of blank input, exit conditional statement
+else:                                                                           # executes if non-blank string received
+    words = words.strip()                                                       # removes whitespace to save iteration times
+    for letter in words.lower():                                                # iterate by each letter over the words given
+        if letter in vowels:                                                    # check if the letter is a vowel
+            vowelcount += 1                                                     # if a vowel is found, the counter is incremented
+    print(f"There are {vowelcount} vowels in the given statement.")             # inform users of the number of vowels
